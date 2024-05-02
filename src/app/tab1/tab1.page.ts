953a0483b231db00169eed9a2585bdf30b5862c8
Dylan Welch <dwelch@aainsco.com>
2024-05-02 15:14:17 -0400

    initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import homeData from '../../assets/data/home.json';
+import { RefresherCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  videos: any [] = [];
+  segments: any[] = [];
 
-  constructor() {}
+  constructor() {
+    this.segments = [
+      'All',
+      'New to you',
+      'Gaming',
+      'Sitcoms',
+      'Computrer program',
+      'Documentarty',
+      'Music',
+    ].map((val) => ({
+      title: val,
+      selected: false,
+    }));
 
+    setTimeout(() => {
+      this.selectSegment(0);
+      this.segments = homeData;
+    }, 1000);
+ }
+
+  selectSegment(index) {
+    this.segments.map((item) => (item.selected = false));
+    this.segments[index].selected = true;
+  }
+
+  doRefresh(event: RefresherCustomEvent) {
+    setTimeout(() => {
+      event.target.complete();
+    }, 1500);
+  }
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { SheetPage } from '../sheet/sheet.page';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(private modalControl: ModalController) {}
 
+  async add() {
+    const modal = await this.modalControl.create({
+      component: SheetPage,
+      breakpoints: [0.5],
+      initialBreakpoint: 0.5,
+      handle: false,
+    })
+    await modal.present();
+  }
 }
